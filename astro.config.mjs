4ce55e3a02f9d093f74a749c9edde6114d206828
Jorge <jorestuard@gmail.com>
2024-11-21 23:07:46 +0100

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://bucolic-moxie-749d6d.netlify.app"
+});
